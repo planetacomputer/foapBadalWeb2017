150435ba2be1d5f825d3616d8bb59031ed2b206e
planetacomputer <evapardeza123@gmail.com>
2017-03-31 14:42:13 +0200

    cookies, taules, json, ajax

--- javascript/cookieLib.js
+++ javascript/cookieLib.js
@@ -32,3 +32,10 @@ function checkCookie() {
        }
     }
 }
+
+RGBToHex = function(r,g,b){
+    var bin = r << 16 | g << 8 | b;
+    return (function(h){
+        return new Array(7-h.length).join("0")+h
+    })(bin.toString(16).toUpperCase())
+}
