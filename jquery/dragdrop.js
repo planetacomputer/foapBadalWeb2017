afdc9024ba79c38405ec94166c512e90daa575ff
planetacomputer <evapardeza123@gmail.com>
2017-04-19 14:02:57 +0200

    DnDrop

---
+++ jquery/dragdrop.js
@@ -0,0 +1,26 @@
+
+var fuente1, soltar;
+function iniciar(){
+  fuente1 = document.getElementById('imagen');
+  fuente1.addEventListener('dragstart', arrastrado);
+ 
+  soltar = document.getElementById('cajaobjetivo');
+  soltar.addEventListener('dragenter', function(e){ e.preventDefault(); console.log("dragenter")});
+  soltar.addEventListener('dragover', function(e){ e.preventDefault(); console.log("dragover")});
+  soltar.addEventListener('dragleave', function(e){ e.preventDefault(); console.log("dragleave")});
+  soltar.addEventListener('drop', soltado);
+}
+function arrastrado(e){
+  console.log("Dragstart arrastrado");
+  var codigo = fuente1.getAttribute('id');
+  e.dataTransfer.setData('Text', codigo);
+}
+function soltado(e){
+  e.preventDefault();
+
+  //soltar.innerHTML = e.dataTransfer.getData('Text');
+  var data = e.dataTransfer.getData("Text");
+  soltar.innerHTML = "";
+  soltar.appendChild(document.getElementById(data));
+}
+addEventListener('load', iniciar);
