038fa5f76c79d99f26eb041b5251f7ebf3745468
planetacomputer <evapardeza123@gmail.com>
2017-05-25 14:46:57 +0200

    Templating Twig

--- sf_tutorial/src/AppBundle/Controller/DefaultController.php
+++ sf_tutorial/src/AppBundle/Controller/DefaultController.php
@@ -14,19 +14,40 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/home.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('default/home.html.twig');
     }
 
     /**
-     * @Route("/about", name="about")
+     * @Route("/about_us", name="about")
      */
     public function aboutAction(Request $request){
         // replace this example code with whatever you need
-        return $this->render('default/about.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('default/about.html.twig');
+    }
+
+    /**
+     * @Route("/menu", name="menu")
+     */
+    public function menuAction(Request $request){
+        //$pdo = new \PDO($dsn, $username, $password);
+        $logger = $this->get('logger');
+        try{
+            $dbh = new \PDO("mysql:host=localhost;dbname=".$this->container->getParameter('database_name'),$this->container->getParameter('database_user'), $this->container->getParameter('database_password'), array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+            $day_number = date('w');
+            $logger->info($day_number);
+            $sql = "SELECT * FROM menu WHERE diaset = :day_number";
+            $query = $dbh->prepare($sql);
+            $query -> bindParam(':day_number', $day_number, \PDO::PARAM_INT);
+            $query->execute();
+
+            $results=$query->fetchAll(\PDO::FETCH_OBJ);
+
+        }
+        catch (PDOException $e){
+            exit("Error: " . $e->getMessage());
+        }
+        // replace this example code with whatever you need
+        return $this->render('default/menu.html.twig', array('menu' => $results));
     }
 
     /**
@@ -34,8 +55,15 @@ class DefaultController extends Controller
      */
     public function whereAction(Request $request){
         // replace this example code with whatever you need
-        return $this->render('default/where.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('default/where.html.twig');
+    }
+
+
+    /**
+     *  @Route("/menu/{id}", name="menu_show", requirements={"id": "\d+"})
+     */
+    public function showMenu(Request $request){
+        echo $request->get('id');
+        return $this->render('default/menuProducte.html.twig');
     }
 }
